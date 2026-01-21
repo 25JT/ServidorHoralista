dfa0c1f08319a83ddb38ff992174c7c0405778f5
25JT <jhonierlucumimeza@gmail.com>
2026-01-21 10:20:42 -0500

    Update Ruta.js

--- src/RutaFront/Ruta.js
+++ src/RutaFront/Ruta.js
@@ -1,2 +1,2 @@
-export const RutaFront = "http://localhost:4321";
-//export const RutaFront = "https://fromprueba-production.up.railway.app";
+//export const RutaFront = "http://localhost:4321";
+export const RutaFront = "https://fromprueba-production.up.railway.app";

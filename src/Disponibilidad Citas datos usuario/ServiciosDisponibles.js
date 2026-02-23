ccd584366b19e661ee5383299f15afdd62db61ec
25JT <jhonierlucumimeza@gmail.com>
2026-02-23 12:28:34 -0500

    Update ServiciosDisponibles.js

--- src/Disponibilidad Citas datos usuario/ServiciosDisponibles.js
+++ src/Disponibilidad Citas datos usuario/ServiciosDisponibles.js
@@ -4,8 +4,37 @@ import bd from "../config/Bd.js";
 
 app.get("/serviciosDisponibles", async (req, res) => {
     try {
-        const [rows] = await bd.query("SELECT nombre_establecimiento, telefono_establecimiento, direccion, hora_inicio, hora_fin, dias_trabajo, Servicio, precio, id FROM pservicio");
+        const [rows] = await bd.query("SELECT nombre_establecimiento, telefono_establecimiento, direccion, hora_inicio, hora_fin, dias_trabajo, Servicio, precio, id, descripcion, logo, banner FROM pservicio");
         res.json({
+            success: true,
+            data: rows,
+        });
+    } catch (error) {
+        console.error("Error al mostrar las citas:", error);
+        res.status(500).json({
+            success: false,
+            message: "Error al mostrar las citas",
+            error: error.message,
+        });
+    }
+});
+
+app.get("/catalogo/vista/usuario/:id", async (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const existeId = await bd.query("SELECT id_pservicio FROM catalogos WHERE id_pservicio= ?", [id]);
+        if (existeId.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No existe el id",
+            });
+        }
+
+        const [rows] = await bd.query("SELECT nombre_servicio, precio, duracion,descripcion, foto1, foto2,foto3, id_pservicio , id FROM catalogos WHERE id_pservicio= ?", [id]);
+
+
+        res.status(200).json({
             success: true,
             data: rows,
         });

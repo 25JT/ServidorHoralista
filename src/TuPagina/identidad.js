90bc43738abc6e26399cf5725a92a110f2e0a7fe
25JT <jhonierlucumimeza@gmail.com>
2026-02-27 12:26:31 -0500

    Update identidad.js

--- src/TuPagina/identidad.js
+++ src/TuPagina/identidad.js
@@ -6,7 +6,7 @@ app.get("/api/tienda/identidad", verificarSesion, async (req, res) => {
 
     const userid = req.session.userId;
     try {
-        const [rows] = await bd.query("select nombre_establecimiento,direccion,descripcion,logo, banner from pservicio where id_usuario = ? ;", [userid])
+        const [rows] = await bd.query("select nombre_establecimiento, telefono_establecimiento, direccion,descripcion,logo, banner from pservicio where id_usuario = ? ;", [userid])
         res.status(200).json({ succes: true, rows })
     } catch {
         res.status(500).json({ succes: false, message: "Error al obtener la identidad" })
